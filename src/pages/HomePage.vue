55e62edeb7536121576398e47556d4eecd9152e0
Nathan Quam <Nathan.M.Quam@gmail.com>
2021-03-12 14:11:19 -0700

    init

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
+    <!-- <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo">
+    <h1 class="my-5 bg-dark text-light p-3 rounded d-flex align-items-center">
+      <span class="mx-2 text-white">Vue 3 Starter</span>
+    </h1> -->
+    <div class="container-fluid h-100">
+      <div class="row">
+        <div class="col">
+          <p>test</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped lang="scss">
+.home{
+  text-align: center;
+  user-select: none;
+  > img{
+    height: 200px;
+    width: 200px;
+  }
+}
+</style>
